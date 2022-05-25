5fba5426c5a9e92f589fbd720ca0f21d2d29d862
sajidur319370 <sajidurrahmansahib@gmail.com>
2022-05-25 22:32:40 +0600

    user data added

--- index.js
+++ index.js
@@ -15,11 +15,30 @@ const client = new MongoClient(uri, {
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1,
 });
+// ========Token========
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden Access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+// ========Token========
+
+
 
 async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db("long-drive").collection("tools");
+        const usersCollection = client.db("long-drive").collection("users");
         // get all tools
         app.get("/tool", async (req, res) => {
             const query = {};
@@ -34,7 +53,25 @@ async function run() {
             const purchase = await toolsCollection.findOne(query);
             res.send(purchase);
         })
+        // ========TokenUser========
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const option = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, option);
+            const token = jwt.sign(
+                { email: email },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "1d" }
+            );
+            res.send({ result, token });
+        });
 
+        // ========TokenUser========
 
     } finally {
     }
