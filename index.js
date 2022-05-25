527c58b6c59666aa9c27456f5fd2f88273389588
sajidur319370 <sajidurrahmansahib@gmail.com>
2022-05-25 10:59:49 +0600

    tool data added in db

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kr5fm.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
     try {
         await client.connect();
-        console.log("Database connected");
-
+        const toolsCollection = client.db("long-drive").collection("tools");
+        app.get("/tool", async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        });
     } finally {
-
     }
-
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-    res.send('Hello Driver!')
-})
+app.get("/", (req, res) => {
+    res.send("Hello Driver!");
+});
 
 app.listen(port, () => {
-    console.log(`Long Drive app listening on port ${port}`)
-})
+    console.log(`Long Drive app listening on port ${port}`);
+});
