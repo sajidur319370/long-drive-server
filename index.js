490a6b98a402475902c069c802d010341a65bf5d
sajidur319370 <sajidurrahmansahib@gmail.com>
2022-05-27 01:51:01 +0600

    payment added in db

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 
 app.use(cors());
 app.use(express.json());
@@ -38,6 +40,7 @@ async function run() {
         const toolsCollection = client.db("long-drive").collection("tools");
         const usersCollection = client.db("long-drive").collection("users");
         const orderCollection = client.db("long-drive").collection("orders");
+        const paymentsCollection = client.db("long-drive").collection("payments");
 
         // ==TokenUser==
         app.put("/user/:email", async (req, res) => {
@@ -57,6 +60,20 @@ async function run() {
             res.send({ result, token });
         });
         // ==TokenUser==
+
+        // ===Payment Intent===
+        app.post("/create-payment-intent", async (req, res) => {
+            const orderPay = req.body;
+            const price = parseInt(orderPay.price) * parseInt(orderPay.orderQuantity);
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+            res.send({ clientSecret: paymentIntent.client_secret });
+        });
+        // ===Payment Intent===
 
         // get all tools
         app.get("/tool", async (req, res) => {
@@ -85,12 +102,38 @@ async function run() {
             const orders = await orderCollection.find().toArray();
             res.send(orders);
         });
+        // get single ordered tool
+        app.get("/order/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const orders = await orderCollection.findOne(filter);
+            res.send(orders);
+        });
         // Delete order from db
         app.delete("/order/:id", async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(filter);
             res.send(result);
+        });
+
+        // set payment status
+        app.patch("/order/:id", async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId,
+                },
+            };
+            const result = await paymentsCollection.insertOne(payment);
+            const updatedOrder = await orderCollection.updateOne(
+                filter,
+                updateDoc
+            );
+            res.send(updateDoc);
         });
     } finally {
     }
